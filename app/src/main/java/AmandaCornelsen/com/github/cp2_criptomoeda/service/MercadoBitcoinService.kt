aafefefdebaa2e603113ac4bf1c866e19903c004
AmandaCornelsen <cornelsen089@gmail.com>
2025-04-29 22:36:24 -0300

    Criando as interfaces

---
+++ app/src/main/java/AmandaCornelsen/com/github/cp2_criptomoeda/service/MercadoBitcoinService.kt
@@ -0,0 +1,11 @@
+package AmandaCornelsen.com.github.cp2_criptomoeda.service
+
+import AmandaCornelsen.com.github.cryptomonitor.model.TickerResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MercadoBitcoinService {
+
+    @GET("api/BTC/ticker/")
+    suspend fun getTicker(): Response<TickerResponse>
+}
