170ee844691366e5192cc9eba52f9394dc8655af
AmandaCornelsen <cornelsen089@gmail.com>
2025-04-29 21:27:53 -0300

    first commit

---
+++ app/src/main/java/AmandaCornelsen/com/github/cp2_criptomoeda/MainActivity.kt
@@ -0,0 +1,43 @@
+package AmandaCornelsen.com.github.cp2_criptomoeda
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import AmandaCornelsen.com.github.cp2_criptomoeda.ui.theme.Cp2_CriptoMoedaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Cp2_CriptoMoedaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Cp2_CriptoMoedaTheme {
+        Greeting("Android")
+    }
+}
